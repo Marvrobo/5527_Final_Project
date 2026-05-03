2f37dbb82fff495a7364b66dc45481abf95d121e
zou00189@umn.edu <mhzou@LAPTOP-N4F03TP8.localdomain>
2026-05-02 22:44:59 -0500

    remove uncessary part

--- GNN_diffusion_policy_vel/model/interactive_gnn.py
+++ GNN_diffusion_policy_vel/model/interactive_gnn.py
@@ -35,25 +35,7 @@ class MLP(nn.Module):
     
     def forward(self, x):
         return self.net(x)
-
-
-def quat_inverse(q):
-    q_inv = q.clone()
-    q_inv[:, :3] = -q_inv[:, :3]
-    return q_inv
-
-
-def quat_mul(q1, q2):
-    x1, y1, z1, w1 = q1[:, 0], q1[:, 1], q1[:, 2], q1[:, 3]
-    x2, y2, z2, w2 = q2[:, 0], q2[:, 1], q2[:, 2], q2[:, 3]
     
-    x = w1*x2 + x1*w2 + y1*z2 - z1*y2
-    y = w1*y2 - x1*z2 + y1*w2 + z1*x2
-    z = w1*z2 + x1*y2 - y1*x2 + z1*w2
-    w = w1*w2 - x1*x2 - y1*y2 - z1*z2
-    return torch.stack([x, y, z, w], dim=-1)
-
-
 class InteractiveGNN(nn.Module):
     def __init__(self, node_dim=TODO, edge_dim=TODO, hidden_dim=TODO, out_dim=TODO):
         super().__init__()
